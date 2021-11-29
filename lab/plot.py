8393a807509df7d6bf15b5c9fa1cb7021e996b52
Ilya Trubachev <trubachev.ii@phystech.edu>
2021-11-29 03:09:22 +0300

    upd

---
+++ lab/plot.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import numpy  as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt("res.dat")
+workers = data[:, 0]
+time = data[:, 1]
+
+first_time = time[0]
+acceleration = first_time / time
+efficienty = acceleration / workers
+
+plt.plot(workers, acceleration)
+plt.plot(workers, efficienty)
+
+plt.show()
